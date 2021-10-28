dc7bc52c0c99ddbb68bff996023f25f597400839
anaflavia_p7 <ana.pesseti@dbserver.com.br>
2021-10-28 15:25:16 -0300

    feat: create reader for qrcode

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,9 @@
 import Link from 'next/link';
 import styles from '../styles/home.module.css';
-import QRCodeReader from '../components/QRCodeReader';
-import { useState } from 'react';
 
 export default function Home() {
-  const [result, setResult] = useState('No result');
-
   return (
     <div className={styles.container}>
-      <h1>{result}</h1>
       <div className={styles.buttonContent}>
         <Link href="/generator">
           <a title="gerar-novo-qrcode">
@@ -33,8 +28,6 @@ export default function Home() {
             </button>
           </a>
         </Link>
-
-        <QRCodeReader result={result} setResult={setResult}/>
       </div>
     </div>
   )
