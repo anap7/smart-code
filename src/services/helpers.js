7ef04a8471eb5a67ea9274d32e96695cfea08a3a
anap7 <ana.flaviap7@hotmail.com>
2021-11-16 22:20:58 -0300

    fix: use moment to change date

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -1,65 +1,70 @@
+import moment from "moment-timezone";
+
 export function returnDateTime(date) {
   const currentDate = Date.parse(date);
 
   let month = currentDate.getMonth() + 1;
-  month = month < 10 ? '0' + month : month;
+  month = month < 10 ? "0" + month : month;
 
   let day = currentDate.getDate();
-  day = day < 10 ? '0' + day : day;
+  day = day < 10 ? "0" + day : day;
 
   const dateFormated = `${day}/${month}/${currentDate.getFullYear()}`;
-  const time = date.split('T')[1];
+  const time = date.split("T")[1];
 
   return {
     date: dateFormated,
     time: time,
-    completedDate: `${dateFormated}-${time}`
-  }
+    completedDate: `${dateFormated}-${time}`,
+  };
 }
 
 export function createCompletedDate() {
-  const currentDate = new Date();
-  const numberDate = Date.parse(currentDate);
-  const currentDateFormated = new Date(numberDate).toLocaleDateString('pt-BR');
-  const currentTimeFormated = new Date(numberDate).toLocaleTimeString('pt-BR', { hour12: false, hour: "numeric", minute: "numeric", second: "numeric"});
+  const currentDate = moment().tz("America/Sao_Paulo").format();
+  const date = moment(currentDate).format('DD/MM/YYYY HH:mm:ss');
 
-  return `${currentDateFormated} ${currentTimeFormated}`;
+  return date;
 }
 
 export function isNumeric(str) {
   let er = /^[0-9]+$/;
-  return (er.test(str));
+  return er.test(str);
 }
 
-export async function register(data, setStatus, setSrc, setRandomOrderNumber, setCodeNumber) {
-
+export async function register(
+  data,
+  setStatus,
+  setSrc,
+  setRandomOrderNumber,
+  setCodeNumber
+) {
   const qrCodeValue = data.qrCodeValue;
-  let type = '';
+  let type = "";
 
   if (qrCodeValue === "" || qrCodeValue === null) {
     setStatus((prevState) => ({
       ...prevState,
       isThereError: true,
       wasSucess: false,
-      description: "O número do QRCode é um valor inválido."
+      description: "O número do QRCode é um valor inválido.",
     }));
 
     return false;
   }
 
-  if (qrCodeValue.includes('http', 'https')) {
-    type = 'url';
+  if (qrCodeValue.includes("http", "https")) {
+    type = "url";
   } else {
-    type = 'number';
+    type = "number";
     if (!qrCodeValue.includes("QR")) {
       setStatus((prevState) => ({
         ...prevState,
         isThereError: true,
         wasSucess: false,
-        description: "O número do QRCode é um valor inválido."
+        description: "O número do QRCode é um valor inválido.",
       }));
 
-      setSrc('');
+      setSrc("");
       setRandomOrderNumber(null);
       return false;
     }
@@ -70,33 +75,34 @@ export async function register(data, setStatus, setSrc, setRandomOrderNumber, se
       ...prevState,
       isThereError: true,
       wasSucess: false,
-      description: "O número do pedido é um valor inválido."
+      description: "O número do pedido é um valor inválido.",
     }));
 
-    setSrc('');
+    setSrc("");
     setRandomOrderNumber(null);
 
     return false;
   }
 
   const result = await fetch(`/api/insert-order`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({submitValues: data, type})
-  })
-    .then(res => res.json());
+    body: JSON.stringify({ submitValues: data, type }),
+  }).then((res) => res.json());
 
   if (result.error || !result) {
     setStatus((prevState) => ({
       ...prevState,
       isThereError: true,
       wasSucess: false,
-      description: result?.error ? result.error : "Ocorreu algum problema ao registrar o novo pedido."
+      description: result?.error
+        ? result.error
+        : "Ocorreu algum problema ao registrar o novo pedido.",
     }));
 
-    setSrc('');
+    setSrc("");
     setRandomOrderNumber(null);
 
     return false;
@@ -106,7 +112,7 @@ export async function register(data, setStatus, setSrc, setRandomOrderNumber, se
     ...prevState,
     isThereError: false,
     wasSucess: true,
-    description: "Seu pedido foi registrado com sucesso!"
+    description: "Seu pedido foi registrado com sucesso!",
   }));
 
   setSrc(result.originalQRCode);
@@ -116,46 +122,52 @@ export async function register(data, setStatus, setSrc, setRandomOrderNumber, se
   return true;
 }
 
-export async function update(data, setStatus, setSrc, setRandomOrderNumber, setCodeNumber) {
-
+export async function update(
+  data,
+  setStatus,
+  setSrc,
+  setRandomOrderNumber,
+  setCodeNumber
+) {
   const inputValue = data.inputValue;
 
-  let type = '';
+  let type = "";
 
-  inputValue.includes('http', 'https') ? type = 'url' : type = 'number';
+  inputValue.includes("http", "https") ? (type = "url") : (type = "number");
 
   if (!isNumeric(data.orderNumber)) {
     setStatus((prevState) => ({
       ...prevState,
       isThereError: true,
       wasSucess: false,
-      description: "O número do pedido é um valor inválido."
+      description: "O número do pedido é um valor inválido.",
     }));
 
-    setSrc('');
+    setSrc("");
     setRandomOrderNumber(null);
 
     return false;
   }
 
   const result = await fetch(`/api/update-order`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({submitValues: data, type})
-  })
-    .then(res => res.json());
+    body: JSON.stringify({ submitValues: data, type }),
+  }).then((res) => res.json());
 
   if (result.error || !result) {
     setStatus((prevState) => ({
       ...prevState,
       isThereError: true,
       wasSucess: false,
-      description: result?.error ? result.error : "Ocorreu algum problema ao registrar o novo pedido."
+      description: result?.error
+        ? result.error
+        : "Ocorreu algum problema ao registrar o novo pedido.",
     }));
 
-    setSrc('');
+    setSrc("");
     setRandomOrderNumber(null);
 
     return false;
@@ -165,7 +177,7 @@ export async function update(data, setStatus, setSrc, setRandomOrderNumber, setC
     ...prevState,
     isThereError: false,
     wasSucess: true,
-    description: "Seu pedido foi registrado com sucesso!"
+    description: "Seu pedido foi registrado com sucesso!",
   }));
 
   setSrc(result.originalQRCode);
@@ -176,49 +188,48 @@ export async function update(data, setStatus, setSrc, setRandomOrderNumber, setC
 }
 
 export async function findOrder(inputValue, setStatus, setFoundNumber) {
+  let type = "";
 
-  let type = '';
-
-  inputValue.includes('http', 'https') ? type = 'url' : type = 'number';
+  inputValue.includes("http", "https") ? (type = "url") : (type = "number");
 
   if (inputValue === "" || inputValue === null) {
     setStatus((prevState) => ({
       ...prevState,
       isThereError: true,
       wasSucess: false,
-      description: "O número de verificação é um valor inválido."
+      description: "O número de verificação é um valor inválido.",
     }));
 
     setFoundNumber((prevState) => ({
       ...prevState,
       wasFound: false,
-      content: {}
+      content: {},
     }));
     return false;
   }
 
   const result = await fetch(`/api/get-order`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({ inputValue, type, typeSearch: 'verification' })
-  })
-    .then(res => res.json());
-
+    body: JSON.stringify({ inputValue, type, typeSearch: "verification" }),
+  }).then((res) => res.json());
 
   if (result.error || !result || !result?.searchResult) {
     setStatus((prevState) => ({
       ...prevState,
       isThereError: true,
       wasSucess: false,
-      description:  result?.error ? result.error : "Ocorreu durante a busca do pedido."
+      description: result?.error
+        ? result.error
+        : "Ocorreu durante a busca do pedido.",
     }));
 
     setFoundNumber((prevState) => ({
       ...prevState,
       wasFound: false,
-      content: {}
+      content: {},
     }));
 
     return false;
@@ -229,13 +240,14 @@ export async function findOrder(inputValue, setStatus, setFoundNumber) {
       ...prevState,
       isThereError: true,
       wasSucess: false,
-      description: "Parece que esse QRCode não tem um pedido associado ainda, por favor acesse a página de associar pedido para fazer o processo de atualização."
+      description:
+        "Parece que esse QRCode não tem um pedido associado ainda, por favor acesse a página de associar pedido para fazer o processo de atualização.",
     }));
 
     setFoundNumber((prevState) => ({
       ...prevState,
       wasFound: false,
-      content: {}
+      content: {},
     }));
 
     return false;
@@ -244,15 +256,15 @@ export async function findOrder(inputValue, setStatus, setFoundNumber) {
       ...prevState,
       isThereError: false,
       wasSucess: false,
-      description: ''
+      description: "",
     }));
 
     setFoundNumber((prevState) => ({
       ...prevState,
       wasFound: true,
       content: {
-        ...result?.searchResult
-      }
+        ...result?.searchResult,
+      },
     }));
   }
 
