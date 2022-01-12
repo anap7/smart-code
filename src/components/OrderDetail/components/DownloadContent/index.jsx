89b877c73029b5175cf911c10af2fa2bc7c1aace
anap7 <ana.flaviap7@hotmail.com>
2022-01-12 16:04:13 -0300

    fix: download content responsive

--- src/components/OrderDetail/components/DownloadContent/index.jsx
+++ src/components/OrderDetail/components/DownloadContent/index.jsx
@@ -5,21 +5,24 @@ export default function DownloadContent() {
 
   return (
     <section className={styles.downloadContent}>
-      <span className={styles.title}>Nota Fiscal: </span>
-      <a title="nfDownload" href='/certifQA.pdf' download>
-        <button className={styles.downloadButton} style={{ marginRight: '1em' }}>
-          <FaDownload />
-          <span style={{marginLeft: '.5em'}}>PDF</span>
-        </button>
-      </a>
-      <span className={styles.title}>Certificado de Qualidade: </span>
-      
-      <a title="certiDownload" href='/certifQA.pdf' download>
-        <button className={styles.downloadButton}>
-        <FaDownload />
-        <span style={{marginLeft: '.5em'}}>PDF</span>
-      </button>
-      </a> 
+      <div className={styles.nfContent}>
+        <span className={styles.title}>Nota Fiscal: </span>
+        <a title="nfDownload" href='/certifQA.pdf' download>
+          <button className={`${styles.downloadButton} ${styles.nfButton}`}>
+            <FaDownload />
+            <span style={{ marginLeft: '.5em' }}>PDF</span>
+          </button>
+        </a>
+      </div>
+      <div className={styles.certicationContent}>
+        <span className={styles.title}>Certificado de Qualidade: </span>
+        <a title="certiDownload" href='/certifQA.pdf' download>
+          <button className={styles.downloadButton}>
+            <FaDownload />
+            <span style={{ marginLeft: '.5em' }}>PDF</span>
+          </button>
+        </a>
+      </div>
     </section>
   )
 }
